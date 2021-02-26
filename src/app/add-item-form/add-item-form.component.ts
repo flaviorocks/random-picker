8b1fe2b12c34240afd3a41cd7d19ed97b5ab8e2d
Flavio Moreira <flavio.moreira@bugleteam.com>
2021-02-26 19:18:07 +0000

    feature: Validate form

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,20 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ListItem } from '../item/list-item';
 
 @Component({
   selector: 'app-add-item-form',
   templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+  styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent {
   @Output() newItem: EventEmitter<ListItem> = new EventEmitter<ListItem>();
 
   addItemForm = new FormGroup({
-    text: new FormControl(''),
+    text: new FormControl('', [
+      Validators.required,
+    ]),
   });
 
   onSubmit() {
+    if (this.addItemForm.invalid) {
+      return;
+    }
+
     const listItem = new ListItem(this.addItemForm.get('text')?.value);
     this.newItem.emit(listItem);
     this.addItemForm.reset();
